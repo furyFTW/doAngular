f817343165b4349ba6dd79e9267fd2df31a62b0f
Edik Bastin <edikbastin@gmail.com>
2015-02-19 13:32:59 +0200

    create project

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# sushi
+# doAngular
--- app/app.js
+++ app/app.js
@@ -1,32 +1,53 @@
-var dep = ['ngRoute', 'app.item','app.directives'];
-var app = angular.module('app', dep)
-app.config(['$routeProvider', function ($routeProvider) {
-    $routeProvider
-        .when('/menu/:Id', {
-            templateUrl: '/app/components/item.html',
-            controller: 'appItem'
-            //resolve: {
-            //    // I will cause a 1 second delay
-            //    delay: function($q, $timeout) {
-            //        var delay = $q.defer();
-            //        $timeout(delay.resolve, 1000);
-            //        return delay.promise;
-            //    }
-            //}
-        }).
-        otherwise({
-            redirectTo: '/',
-            templateUrl: '/app/components/item.html'
+var dep = ['ngRoute', 'app.item'];
+var app = angular.module('app', dep).
+    service('orderService', function () {
+        var order = [];
+        this.addOrder = function (num) {
+            order.push(num);
+            return order;
+        };
+        this.deleteOrder = function (id) {
+            order = [];
+            return this.list();
+        };
+        this.list = function () {
+            return order;
+        };
 
-        });
-    ;
-}]);
-app.controller('appController', ['$scope', '$http', function ($scope, $http) {
-    $http.get('/sushi/app/data/information.json').
-        success(function (data, status, headers, config) {
-            $scope.menu = data;
-        }).
-        error(function (data, status, headers, config) {
-            // or server returns response with an error status.
-        });
-}]);
+    }).
+    controller('appController', ['$scope', '$http', 'orderService', function ($scope, $http, orderService) {
+        $scope.myOrder = orderService.list();
+        $http.get('/sushi/app/data/information.json').
+            success(function (data) {
+                $scope.menu = data;
+            }).
+            error(function (data) {
+                // or server returns response with an error status.
+            });
+        $scope.delete = function () {
+            $scope.myOrder = orderService.deleteOrder();
+        };
+        $scope.addToCard = function (id) {
+            $scope.myOrder = orderService.addOrder(id);
+            console.log($scope.myOrder)
+        };
+    }]);
+
+//app.config(['$routeProvider', function ($routeProvider) {
+//    $routeProvider
+//        .when('/menu/:Id', {
+//            templateUrl: '/app/components/item.html',
+//            controller: 'appItem'
+//            //resolve: {
+//            //    // I will cause a 1 second delay
+//            //    delay: function($q, $timeout) {
+//            //        var delay = $q.defer();
+//            //        $timeout(delay.resolve, 1000);
+//            //        return delay.promise;
+//            //    }
+//            //}
+//        }).
+//        otherwise({
+//            redirectTo: '/'
+//        });
+//}]);
